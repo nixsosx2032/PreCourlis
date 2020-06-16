a1395fe80e7b0451bf17108429dc7d3bc0812036
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2020-06-16 18:50:16 +0200

    Add layer and profile selection

--- PreCourlis/widgets/profile_dialog.py
+++ PreCourlis/widgets/profile_dialog.py
@@ -1,12 +1,33 @@
 from pkg_resources import resource_filename
 
-from PyQt5 import uic
-from PyQt5 import QtWidgets
+from qgis.core import (
+    QgsFeatureRequest,
+    QgsMapLayer,
+    QgsProject,
+    QgsWkbTypes,
+)
+from qgis.PyQt import QtGui, QtWidgets, uic
+from qgis.utils import iface
 
 # This loads your .ui file so that PyQt can populate your plugin with the elements from Qt Designer
 FORM_CLASS, _ = uic.loadUiType(
     resource_filename("PreCourlis", "ui/profile_dialog_base.ui")
 )
+
+
+class ProfileComboxModel(QtGui.QStandardItemModel):
+    def setLayer(self, layer):
+        self.clear()
+        if layer is None:
+            return
+
+        sec_name_index = layer.fields().indexFromName("sec_name")
+        request = QgsFeatureRequest()
+        request.addOrderBy("sec_id")
+        request.setSubsetOfAttributes([sec_name_index])
+        for f in layer.getFeatures(request):
+            item = QtGui.QStandardItem(f.attribute(sec_name_index))
+            self.appendRow(item)
 
 
 class ProfileDialog(QtWidgets.QDialog, FORM_CLASS):
@@ -19,3 +40,42 @@ class ProfileDialog(QtWidgets.QDialog, FORM_CLASS):
         # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
         # #widgets-and-dialogs-with-auto-connect
         self.setupUi(self)
+
+        self.profileComboBoxModel = ProfileComboxModel(self)
+
+        self.init_layer_combo_box()
+        self.init_profiles_combo_box()
+
+        self.layer_changed(self.layer())
+
+    def init_layer_combo_box(self):
+        def accept_layer(layer):
+            if layer.type() != QgsMapLayer.VectorLayer:
+                return False
+            if layer.geometryType() != QgsWkbTypes.LineGeometry:
+                return False
+            if layer.fields().indexFromName("sec_id") == -1:
+                return False
+            return True
+
+        excluded_layers = []
+        for layer in QgsProject.instance().mapLayers().values():
+            if not accept_layer(layer):
+                excluded_layers.append(layer)
+        self.layerComboBox.setExceptedLayerList(excluded_layers)
+
+        # Select active layer
+        layer = iface.activeLayer()
+        if layer not in excluded_layers:
+            self.layerComboBox.setLayer(layer)
+
+        self.layerComboBox.layerChanged.connect(self.layer_changed)
+
+    def init_profiles_combo_box(self):
+        self.profileComboBox.setModel(self.profileComboBoxModel)
+
+    def layer_changed(self, layer):
+        self.profileComboBoxModel.setLayer(layer)
+
+    def layer(self):
+        return self.layerComboBox.currentLayer()
--- test/widgets/test_profile_dialog.py
+++ test/widgets/test_profile_dialog.py
@@ -1,6 +1,25 @@
+import os
 import unittest
+from unittest.mock import patch
+
+from qgis.core import QgsProject, QgsVectorLayer
+from qgis.testing.mocked import get_iface as get_iface_base
 
 from PreCourlis.widgets.profile_dialog import ProfileDialog
+
+from .. import DATA_PATH
+
+PROFILES_PATH = os.path.join(DATA_PATH, "input", "profiles_lines.shp")
+
+
+def get_iface():
+    project = QgsProject.instance()
+    layer = QgsVectorLayer(PROFILES_PATH, "profiles_lines", "ogr")
+    project.addMapLayer(layer)
+
+    iface = get_iface_base()
+    iface.activeLayer.return_value = layer
+    return iface
 
 
 class TestProfileDialog(unittest.TestCase):
@@ -9,6 +28,7 @@ class TestProfileDialog(unittest.TestCase):
     def create_dialog(self):
         return ProfileDialog(None)
 
+    @patch("PreCourlis.widgets.profile_dialog.iface", get_iface())
     def test_init(self):
         """Test we can click OK."""
         self.dialog = self.create_dialog()
