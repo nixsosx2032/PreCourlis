1fccf7ffeabd36cdca89dad2bd086e40b651159b
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2020-07-31 14:15:28 +0200

    Add test for layers colors

--- test/core/test_precourlis_file.py
+++ test/core/test_precourlis_file.py
@@ -2,6 +2,7 @@ import os
 import unittest
 
 from qgis.core import QgsVectorLayer
+from qgis.PyQt import QtGui
 
 from PreCourlis.core.precourlis_file import PreCourlisFileLine
 
@@ -9,6 +10,11 @@ from .. import PROFILE_LINES_PATH, EXPECTED_PATH, TEMP_PATH, OVERWRITE_EXPECTED,
 
 
 class TestPreCourlisFileLine(unittest.TestCase):
+    def create_file(self):
+        layer = QgsVectorLayer(PROFILE_LINES_PATH, "profiles", "ogr")
+        assert layer.isValid()
+        return PreCourlisFileLine(layer)
+
     def test_add_sedimental_layer(self):
         output_path = os.path.join(TEMP_PATH, "add_sedimental_layer.gml")
         expected_path = os.path.join(EXPECTED_PATH, "add_sedimental_layer.gml")
@@ -16,13 +22,11 @@ class TestPreCourlisFileLine(unittest.TestCase):
         if OVERWRITE_EXPECTED:
             output_path = expected_path
 
-        layer = QgsVectorLayer(PROFILE_LINES_PATH, "profiles", "ogr")
-        assert layer.isValid()
+        file = self.create_file()
+        file.layer().startEditing()
+        file.add_sedimental_layer("Layer2", 1)
 
-        layer.startEditing()
-        PreCourlisFileLine(layer).add_sedimental_layer("Layer2", 1)
-
-        output = utils.save_as_gml(layer, output_path)
+        output = utils.save_as_gml(file.layer(), output_path)
         expected = QgsVectorLayer(expected_path, "expected", "ogr")
         assert expected.isValid()
         self.assertLayersEqual(output, expected)
@@ -34,13 +38,17 @@ class TestPreCourlisFileLine(unittest.TestCase):
         if OVERWRITE_EXPECTED:
             output_path = expected_path
 
-        layer = QgsVectorLayer(PROFILE_LINES_PATH, "profiles", "ogr")
-        assert layer.isValid()
+        file = self.create_file()
+        file.layer().startEditing()
+        file.delete_sedimental_layer("Layer1")
 
-        layer.startEditing()
-        PreCourlisFileLine(layer).delete_sedimental_layer("Layer1")
-
-        output = utils.save_as_gml(layer, output_path)
+        output = utils.save_as_gml(file.layer(), output_path)
         expected = QgsVectorLayer(expected_path, "expected", "ogr")
         assert expected.isValid()
         self.assertLayersEqual(output, expected)
+
+    def test_layer_color(self):
+        file = self.create_file()
+        assert file.layer_color("zfond") == "#ff0000"
+        file.set_layer_color("Layer1", QtGui.QColor("#7f7f7f"))
+        assert file.layer_color("Layer1") == "#7f7f7f"
