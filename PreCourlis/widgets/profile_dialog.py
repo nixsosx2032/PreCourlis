2cebe969b358edfaf01b5a3cf6f771d305fba60a
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2020-09-03 08:37:02 +0200

    Move SectionItemModel in a dedicated file

--- PreCourlis/widgets/profile_dialog.py
+++ PreCourlis/widgets/profile_dialog.py
@@ -2,7 +2,6 @@ from pkg_resources import resource_filename
 
 from qgis.core import (
     QgsApplication,
-    QgsFeatureRequest,
     QgsMapLayer,
     QgsProject,
     QgsWkbTypes,
@@ -15,55 +14,13 @@ from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as Navigatio
 from PreCourlis.core.precourlis_file import PreCourlisFileLine
 from PreCourlis.widgets.delegates import FloatDelegate
 from PreCourlis.widgets.points_table_model import PointsTableModel
+from PreCourlis.widgets.section_item_model import SectionItemModel
 from PreCourlis.widgets.sedimental_layer_model import SedimentalLayerModel
 
 # This loads your .ui file so that PyQt can populate your plugin with the elements from Qt Designer
 FORM_CLASS, _ = uic.loadUiType(
     resource_filename("PreCourlis", "ui/profile_dialog_base.ui")
 )
-
-
-class SectionItem(QtGui.QStandardItem):
-    def __init__(self, text, current_f_id, previous_f_id):
-        super().__init__(text)
-        self.current_f_id = current_f_id
-        self.previous_f_id = previous_f_id
-        self.next_f_id = None
-
-    def set_next_f_id(self, next_f_id):
-        self.next_f_id = next_f_id
-
-
-class SectionItemModel(QtGui.QStandardItemModel):
-    def setLayer(self, layer):
-        self.beginResetModel()
-
-        self.clear()
-        if layer is None:
-            return
-
-        sec_name_index = layer.fields().indexFromName("sec_name")
-        request = QgsFeatureRequest()
-        request.addOrderBy("sec_id")
-        request.setSubsetOfAttributes([sec_name_index])
-
-        previous_f_id = None
-        previous_item = None
-
-        for f in layer.getFeatures(request):
-            item = SectionItem(
-                f.attribute(sec_name_index),
-                f.id(),
-                previous_f_id,
-            )
-            if previous_item is not None:
-                previous_item.set_next_f_id(f.id())
-            self.appendRow(item)
-
-            previous_f_id = f.id()
-            previous_item = item
-
-        self.endResetModel()
 
 
 class ProfileDialog(QtWidgets.QDialog, FORM_CLASS):
